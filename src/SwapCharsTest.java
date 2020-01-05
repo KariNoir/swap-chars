2dcdb99835d8e5cd2aa1358807cfd63499db2565
KariNoir <KariNoirm@gmail.com>
2020-01-05 14:06:11 -0500

    add some char1 and test

---
+++ src/SwapCharsTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharsTest {
+
+	@Test
+	void swapStringwith2Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));
+	}
+	void swapStringwith6Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));
+	}
+}
